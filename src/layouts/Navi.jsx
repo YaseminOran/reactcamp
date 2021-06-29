74e40f595ec2c00c65dcf595232e462137bb0ef2
yasemin <oran.yasemin@gmail.com>
2021-06-29 22:18:38 +0300

    initial commit

--- src/layouts/Navi.jsx
+++ src/layouts/Navi.jsx
@@ -5,11 +5,10 @@ import { Button, Container, Menu } from 'semantic-ui-react';
 export default function Navi() {
   return (
     <div>
-      <Menu inverted fixed>
+      <Menu inverted fixed="top">
         <Container>
           <Menu.Item name="home" />
           <Menu.Item name="messages" />
-
           <Menu.Menu position="right">
             <CartSummary />
             <Menu.Item>
